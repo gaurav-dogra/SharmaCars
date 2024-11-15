28b78b34162e68faed577f240e6016d0fe09e540
Gaurav Dogra <dograg@flprod.co.uk>
2024-11-15 18:23:54 +0000

    added CRUD stored procedures for User table

---
+++ Db/StoredProcedures/spUser_Get.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spUser_Get]
+@Id int
+AS
+	begin
+	SELECT Id, FirstName, LastName, UserGroup, [Status] 
+	FROM dbo.[User]
+	WHERE Id = @Id;
+end
---
+++ Db/StoredProcedures/spUser_GetAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spUser_GetAll]
+AS
+begin
+	SELECT Id, FirstName, LastName, UserGroup, [Status] 
+	FROM dbo.[User];
+end

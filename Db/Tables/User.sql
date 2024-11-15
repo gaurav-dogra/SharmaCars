b1ad8bf755955497b3e2874c183aead3688d9bda
Gaurav Dogra <dograg@flprod.co.uk>
2024-11-15 18:18:36 +0000

    added new SQLServer database project, a user table

---
+++ Db/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL, 
+    [UserGroup] INT NOT NULL DEFAULT 1, 
+    [Status] INT NOT NULL DEFAULT 1
+)

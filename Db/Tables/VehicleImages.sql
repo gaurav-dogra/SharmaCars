d9dcbc0364c0ea3ddbb39610c9fffffef57f74e7
Gaurav Dogra <dograg@flprod.co.uk>
2024-11-16 23:55:15 +0000

    added table VehicleImages

---
+++ Db/Tables/VehicleImages.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[VehicleImages]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [VehicleId] INT NOT NULL, 
+    [ImagePath] NVARCHAR(255) NOT NULL, 
+    [IsMainImage] BIT NOT NULL DEFAULT 0,
+    CONSTRAINT FK_VehicleImages_Vehicle FOREIGN KEY (VehicleId) REFERENCES Vehicle(Id),
+    CONSTRAINT CK_OneMainImage CHECK (
+        IsMainImage = 0 OR
+        (SELECT COUNT(*) FROM VehicleImages WHERE VehicleId = VehicleImages.VehicleId AND IsMainImage = 1) <= 1
+    )
+
+
+);

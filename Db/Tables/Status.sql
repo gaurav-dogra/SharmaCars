463e40c36101fc90e720b9ae6ad2dff1fcf193c7
Gaurav Dogra <dograg@flprod.co.uk>
2024-11-17 00:30:32 +0000

    added table Vehicle

---
+++ Db/Tables/Status.sql
@@ -0,0 +1,5 @@
+﻿CREATE TABLE [dbo].[Status]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Status] NVARCHAR(20) NOT NULL
+)
--- Db/Tables/VehicleImages.sql
+++ Db/Tables/VehicleImages.sql
@@ -1,14 +1,11 @@
-﻿CREATE TABLE [dbo].[VehicleImages]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [VehicleId] INT NOT NULL, 
-    [ImagePath] NVARCHAR(255) NOT NULL, 
-    [IsMainImage] BIT NOT NULL DEFAULT 0,
-    CONSTRAINT FK_VehicleImages_Vehicle FOREIGN KEY (VehicleId) REFERENCES Vehicle(Id),
-    CONSTRAINT CK_OneMainImage CHECK (
-        IsMainImage = 0 OR
-        (SELECT COUNT(*) FROM VehicleImages WHERE VehicleId = VehicleImages.VehicleId AND IsMainImage = 1) <= 1
-    )
-
-
+﻿CREATE TABLE VehicleImages (
+	Id INT IDENTITY PRIMARY KEY,
+	VehicleId INT NOT NULL,
+	ImagePath NVARCHAR(255) NOT NULL,
+	IsMainImage BIT NOT NULL DEFAULT 0,
+	CONSTRAINT FK_VehicleImages_Vehicle FOREIGN KEY (VehicleId) REFERENCES Vehicle(Id)
 );
+
+CREATE UNIQUE INDEX UX_OneMainImagePerVehicle 
+ON VehicleImages (VehicleId, IsMainImage) 
+WHERE IsMainImage = 1;

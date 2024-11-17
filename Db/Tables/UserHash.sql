09b0a26b6f934bbebc8df16d9f47eb57fc482abe
Gaurav Dogra <dograg@flprod.co.uk>
2024-11-17 00:47:39 +0000

    added userId column

--- Db/Tables/UserHash.sql
+++ Db/Tables/UserHash.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].UserHash (
     Id INT NOT NULL PRIMARY KEY,
     [Hash] NVARCHAR(100) NOT NULL,
+    [UserId] INT NOT NULL, 
     CONSTRAINT FK_UserHash_Id FOREIGN KEY (Id) REFERENCES [User](Id)
     ON DELETE CASCADE
 );

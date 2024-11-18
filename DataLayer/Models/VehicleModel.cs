69bb8b7bce5f43ec4e03093db5cf4f46b80acf92
Gaurav Dogra <dograg@flprod.co.uk>
2024-11-18 14:15:14 +0000

    added VehicleModel class

---
+++ DataLayer/Models/VehicleModel.cs
@@ -0,0 +1,28 @@
+﻿namespace DataLayer.Models;
+
+public class VehicleModel
+{
+    public int Id { get; set; }
+    public string HeadlineSummary { get; set; }
+    public int Status { get; set; }
+    public string? Model { get; set; }
+    public int? Mileage { get; set; }
+    public int? RegistrationYear { get; set; }
+    public int? Gearbox { get; set; }
+    public int? FuelType { get; set; }
+    public int? Price { get; set; }
+    public int? PreviousOwners { get; set; }
+    public int? ServiceHistory { get; set; }
+    public bool? IsBrandNew { get; set; }
+    public int? BodyType { get; set; }
+    public int? Doors { get; set; }
+    public int? Seats { get; set; }
+    public int? Category { get; set; }
+    public int? EmissionClass { get; set; }
+    public int? EngineCapacity { get; set; }
+    public int? BatteryRange { get; set; }
+    public string? Description { get; set; }
+    public int? Colour { get; set; }
+    public int? DriveType { get; set; }
+
+}

c2b5580ad38e313d247407c07757e64261dcbf29
Gaurav Dogra <dograg@flprod.co.uk>
2024-11-18 22:44:55 +0000

    added IVehicleRepository & VehicleRepository

---
+++ DataLayer/IVehicleRepository.cs
@@ -0,0 +1,13 @@
+﻿using DataLayer.Models;
+
+namespace DataLayer
+{
+    public interface IVehicleRepository
+    {
+        Task<VehicleModel> GetAsync(int id);
+        Task<IEnumerable<VehicleModel>> GetAllAsync();
+        Task<VehicleModel> AddAsync(VehicleModel vehicle);
+        Task<VehicleModel> UpdateAsync(VehicleModel vehicle);
+        void Remove(int id);
+    }
+}
---
+++ DataLayer/VehicleRepository.cs
@@ -0,0 +1,65 @@
+﻿using Dapper;
+using DataLayer.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace DataLayer
+{
+    public class VehicleRepository(string connString) : IVehicleRepository
+    {
+        private IDbConnection _db = new SqlConnection(connString);
+
+
+        public Task<VehicleModel> GetAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<VehicleModel>> GetAllAsync()
+        {
+            using (_db)
+            {
+                const string query = @"Select 
+                                        [Id],
+                                        [HeadlineSummary], 
+                                        [Model], 
+                                        [Mileage], 
+                                        [RegistrationYear], 
+                                        [Gearbox],
+                                        [FuelType], 
+                                        [Price], 
+                                        [PreviousOwners], 
+                                        [ServiceHistory], 
+                                        [BrandNew], 
+                                        [BodyType],
+                                        [Doors], 
+                                        [Seats], 
+                                        [Category], 
+                                        [EmissionClass], 
+                                        [EngineCapacity], 
+                                        [BatteryRange], 
+                                        [Description],
+                                        [Colour], 
+                                        [DriveType] 
+                                    From [dbo].Vehicle";
+                return await _db.QueryAsync<VehicleModel>(query);
+            }
+
+        }
+
+        public Task<VehicleModel> AddAsync(VehicleModel vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<VehicleModel> UpdateAsync(VehicleModel vehicle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

93a59f8013864aba478ac1f0675a64503194ec85
Gaurav Dogra <dograg@flprod.co.uk>
2024-11-18 23:45:09 +0000

    removed unused usings in IGenericRepository

--- DataLayer/Contracts/IGenericRepository.cs
+++ DataLayer/Contracts/IGenericRepository.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DataLayer.Contracts;
+﻿namespace DataLayer.Contracts;
 
 public interface IGenericRepository<T> where T : class
 {

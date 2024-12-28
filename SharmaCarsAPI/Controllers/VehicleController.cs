bfb6560726a924c5078aee1ab7fb0a05be43e306
Gaurav Dogra <dograg@flprod.co.uk>
2024-12-28 23:12:01 +0000

    added GetAll HttpGet method

---
+++ SharmaCarsAPI/Controllers/VehicleController.cs
@@ -0,0 +1,16 @@
+﻿using DataLayer.Contracts;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SharmaCarsAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VehicleController(IUnitOfWork unitOfWork) : Controller
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var data = await unitOfWork.Vehicles.GetAllAsync();
+        return Ok(data);
+    }
+}

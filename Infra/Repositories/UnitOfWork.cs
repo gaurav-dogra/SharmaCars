ea787d230efdb5df4bb7114f03f2015601d4fef1
Gaurav Dogra <dograg@flprod.co.uk>
2024-12-28 23:15:00 +0000

    add UnitOfWork.cs in infra project

---
+++ Infra/Repositories/UnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using DataLayer.Contracts;
+
+namespace Infra.Repositories;
+
+public class UnitOfWork(IVehicleRepository vehicles) : IUnitOfWork
+{
+    public IVehicleRepository Vehicles { get; } = vehicles;
+}
